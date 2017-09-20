83d2c79eb2792aafc9e207caafd86a7e60ab9b4c
ben0815 <ben0815@tamu.edu>
2017-09-20 12:58:22 -0500

    Completed handle GET function. HTML and various image files can now be sent to a connected client.

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -2,8 +2,8 @@
 
 
 http_server::
-http_server(std::string _port, std::string _root_dir) : m_port(_port),
-  m_root_dir(_root_dir) {
+http_server(std::string _port, std::string _root) : m_port(_port),
+  m_root(_root) {
   addrinfo hints;
 
   memset(&hints, 0, sizeof hints);
@@ -62,25 +62,18 @@ run() {
     }
 
     // Continuously receive and serve requests from the client.
-    char* buffer = new char[2048];
-    if(recv(m_accepted, buffer, 2048, 0) == -1) {
+    char* buffer = new char[8912];
+    if(recv(m_accepted, buffer, 8912, 0) == -1) {
       perror("Receive failed");
       throw std::runtime_error("Failed to receive data from accepting socket "
           "with file descriptor " + std::to_string(m_accepted) + ".");
-     }
+    }
 
     const auto request = std::string(buffer);
-    if(request.size() > 1024)
-      throw std::runtime_error("Cannot service request's longer than 1 KB.");
-
-    if(!request.empty())
-      std::cout << request << std::endl;
 
     // Send the request to the parser to make sure it is a GET request.
     // Handle request appropriately.
     parse(request);
-
-  //const char message[] = "HTTP/1.1 200 Okay\r\nContent-Type: text/html; charset=ISO-8859-4 \r\n\r\n<h1> Hi! <h1>";
 
     delete[] buffer;
     close(m_accepted);
@@ -91,11 +84,12 @@ run() {
 void
 http_server::
 parse(const std::string& _request) const {
-  const auto req_line = _request.substr(0, _request.find("\n")),
-             method = req_line.substr(0, req_line.find(" "));
+  const std::string req_line(_request, 0, _request.find("\n")),
+                    method(req_line, 0, req_line.find(" "));
 
-  if(method == "ET")
+  if(method == "GET")
     handle_get_request(req_line);
+  // For this assignment only GET methods are allowed.
   else
     error_response("405 Method Not Allowed");
 }
@@ -104,12 +98,128 @@ parse(const std::string& _request) const {
 void
 http_server::
 handle_get_request(const std::string& _req_line) const {
+  // Get the requested uri.
+  std::string uri = "";
+
+  {
+    const size_t start = _req_line.find(" ") + 1,
+                 num = _req_line.rfind(" ") - start;
+
+    uri = _req_line.substr(start, num);
+  }
+
+  // Keep URIs under 2000 characters. This makes them work on all browsers.
+  if(uri.size() > 2000)
+    error_response("414 URI Too Long");
+
+  // Append the uri to the root to get the path of the requested resource.
+  std::string path = "";
+  if(m_root.at(m_root.size() - 1) == '/')
+    path = m_root.substr(0, m_root.size() - 1) + uri;
+  else
+    path = m_root + uri;
+
+  // If a directory is requested try to serve index.html in that directory.
+  if(uri.at(uri.size() - 1) == '/')
+    path += "index.html";
+
+  // Get the file extension of the requested resource if applicable.
+  std::string extension = "";
+  {
+    const size_t start = path.rfind("."),
+                 num = path.size() - start;
+
+    extension = path.substr(start, num);
+  }
+
+  // Check if we have a valid file extension. Also return the content type.
+  const auto& valid = is_valid_ext(extension);
+
+  if(!valid.first) {
+    error_response("415 Unsupported Media Type");
+    return;
+  }
+
+  // Get the type of content from the validity checker.
+  const auto& content_type = valid.second;
+
+  // If the resource exists and it is valid, serve it.
+  serve(content_type, path);
+}
+
+
+void
+http_server::
+serve(const std::string& _content_type, const std::string& _path) const {
+  const std::string type(_content_type, 0, _content_type.find("/"));
+
+  auto mode = std::fstream::in;
+
+  // If the resource is an image we need to open it in binary mode.
+  if(type == "image")
+    mode |= std::fstream::binary;
+
+  // Try to open resource.
+  std::ifstream ifs(_path, mode);
+  if(!ifs) {
+    error_response("404 Not Found");
+    ifs.close();
+    return;
+  }
+
+  // Read the resource content into a string.
+  const std::string content{std::istreambuf_iterator<char>(ifs),
+      std::istreambuf_iterator<char>()};
+
+  // Construct the HTTP response.
+  const std::string response = "HTTP/1.1 200 OK\r\nServer: Ben's HTTP server.\r\n"
+    "Content-Length: " + std::to_string(content.size()) + "\r\nContent-Type: " +
+    _content_type + "\r\nConnection: Closed\r\n\r\n" + content;
+
+  send(m_accepted, response.c_str(), response.size(), 0);
 }
 
 
 void
 http_server::
 error_response(const std::string& _error) const {
-    const std::string response = "HTTP/1.1 " + _error + " \r\n";
-    send(m_accepted, response.c_str(), response.size(), 0);
+  // Send an error to the client.
+  const std::string response = "HTTP/1.1 " + _error + "\r\n";
+  send(m_accepted, response.c_str(), response.size(), 0);
+}
+
+
+std::pair<bool, std::string>
+http_server::
+is_valid_ext(const std::string& _extension) const {
+  // Whitelist of file extensions. These are the file types that this
+  // server can handle.
+  static std::vector<std::string> white_list{".txt", ".html", ".jpg", "jpeg",
+    ".png", ".ico", ".gif"};
+
+  const auto& it = std::find(white_list.cbegin(), white_list.cend(), _extension);
+
+  // False if we our extension is not in the white list.
+  if(it == white_list.cend())
+    return std::make_pair(false, "");
+
+  std::string content_type = "";
+
+  // If extension is valid determine its content type.
+  if(_extension == ".txt")
+    content_type = "text/plain; charset=utf-8";
+  else if(_extension == ".html")
+    content_type = "text/html; charset=utf-8";
+  else if(_extension == ".jpg" or _extension == ".jpeg")
+    content_type = "image/jpeg";
+  else if(_extension == ".png")
+    content_type = "image/png";
+  else if(_extension == ".ico")
+    content_type = "image/ico";
+  else if(_extension == ".gif")
+    content_type = "image/gif";
+  else
+    throw std::runtime_error("Extension invalid after it passed validity test.");
+
+  return std::make_pair(true, content_type);
 }
--- src/http_server.hpp
+++ src/http_server.hpp
@@ -7,6 +7,11 @@
 #include <unistd.h>
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <iterator>
+#include <algorithm>
 
 
 class http_server {
@@ -16,7 +21,7 @@ class http_server {
     http_server() {}
 
     // Construct and initialize server. Setup address.
-    http_server(std::string _port, std::string _root_dir);
+    http_server(std::string _port, std::string _root);
 
     ~http_server();
 
@@ -30,8 +35,14 @@ class http_server {
     // Handle and respond to GET requests.
     void handle_get_request(const std::string& _req_line) const;
 
+    // Serve a valid file.
+    void serve(const std::string& _content_type, const std::string& _path) const;
+
     // Respond with an error code.
     void error_response(const std::string& _error) const;
+
+    // Check if we can handle the requested file based on its extension.
+    std::pair<bool, std::string> is_valid_ext(const std::string& _extension) const;
 
   private:
 
@@ -43,7 +54,7 @@ class http_server {
 
     std::string m_port{"2490"};
 
-    std::string m_root_dir{"./"};
+    std::string m_root{"./"};
 
     addrinfo* m_addr{nullptr};
 };
